ead3b4700d03f0ddc62f73278e514a2a80b1dc70
Paweł Kieryk <pawel.kieryk@codewise.com>
2019-08-04 16:59:17 +0200

    Add ConfigWizard component

---
+++ src/components/ConfigWizard.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="config-wizard">
+        <div
+            v-for="(step, index) in steps"
+            :key="index"
+            :class="[
+                'config-wizard__step',
+                index === currentStep ? 'config-wizard__step--active' : ''
+            ]"
+        >
+            <div class="config-wizard__step-icon"></div>
+            <p class="config-wizard__step-title">step {{ index }}<br>{{ step }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        steps: {
+            type: Array,
+            required: true
+        },
+        currentStep: {
+            type: Number,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.config-wizard {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:before {
+        position: absolute;
+        content: '';
+        top: 23px;
+        width: 85%;
+        height: 2px;
+        background-color: #EBF7FF;
+    }
+
+    &__step {
+        text-align: center;
+        margin: 0 27px;
+
+        &:first-child {
+            margin-left: 0;
+        }
+
+        &:last-child {
+            margin-right: 0;
+        }
+
+        &--active {
+            .config-wizard__step-icon {
+                background-color: #95A3AD;
+
+                &:before {
+                    background-color: white;
+                }
+            }
+        }
+    }
+
+	&__step-icon {
+        position: relative;
+        display: inline-block;
+        width: 46px;
+        height: 46px;
+        background-color: #CDDBE5;
+        margin-bottom: 11px;
+        border-radius: 100%;
+
+        &:before {
+            position: absolute;
+            content: '';
+            top: calc(50% - 3px);
+            left: calc(50% - 3px);
+            width: 6px;
+            height: 6px;
+            background-color: #6991B2;
+            border-radius: 100%;
+        }
+	}
+
+	&__step-title {
+        font-size: 10px;
+        font-weight: 700;
+        text-transform: uppercase;
+        margin-top: 0;
+        margin-bottom: 0;
+        text-align: center;
+        line-height: 1.35;
+	}
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,21 @@
 <template>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <ConfigWizard
+        :current-step="currentStep"
+        :steps="configSteps"
+    />
 </template>
 
 <script>
-export default {
+import ConfigWizard from '@/components/ConfigWizard.vue';
 
+export default {
+    data: () => ({
+        configSteps: ['choose door', 'choose division', 'choose color'],
+        currentStep: 1
+    }),
+    components: {
+        ConfigWizard
+    }
 }
 </script>
 
